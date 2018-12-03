cca856a961668529e93a60dc3d757291f3278b99
meatloaf <wewladdie@waifu.club>
2018-12-03 14:42:51 -0600

    forgot that twos, threes only count once per line

--- 2018/2/1/sol.c
+++ 2018/2/1/sol.c
@@ -15,25 +15,25 @@ int compare(const void *l, const void *r){
 void check_line(char *line, struct twos_threes* v){
 	int i = 0;
 	int sz = strlen(line);
-	/*bool twoback = false;
-	bool threeback = false;
-	for (i = 2; i < strlen(line); i++){
-		//TODO check
-		twoback = (line[i-2] == line[i]);
-		threeback = (line[i-1] == line[i]);	
-	}*/
+	bool two = false;
+	bool three = false;
 	while(i < sz){
 		if(i + 1 < sz && line[i] == line[i+1]){
 			if (i+2 < sz && line[i] == line[i+2]){
-				v->threes++;
-				i+=2;	
+				if (!three)
+					v->threes++;
+				i+=2;
+				three = true;
 			}
 			else if (line[i] == line[i+1]){
-				v->twos++;
+				if (!two)
+					v->twos++;
 				i++;
+				two = true;
 			}
 		}
 		i++;
+	if (two && three) return;
 	}
 }
 

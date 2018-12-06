795c83a1d16e359ec77df116706400f45ad1bdab
meatloaf <wewladdie@waifu.club>
2018-12-06 11:07:04 -0600

    d4p1 almost done, file parsing complete

---
+++ 2018/4/1/sol.c
@@ -0,0 +1,143 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define FILELINE 934
+#define DELIMS "[-:] #"
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+struct elf_schedule{
+	//note: id may be 0. in an array of schedules,
+	//this means the id is the last-seen non-0 id.
+	unsigned short id;
+	unsigned short year;
+	unsigned char month;
+	unsigned char day;
+	unsigned char hour;
+	unsigned char minute;
+//	bool begin;
+	bool asleep;
+};
+
+int compare_scheds(const void *first, const void *second){
+	struct elf_schedule *f = (struct elf_schedule *)first;
+	struct elf_schedule *s = (struct elf_schedule *)second;
+	int val;
+	val = f->year - s->year;
+	if (val) return val;
+	val = f->month - s->month;
+	if (val) return val;
+	val = f->day - s->day;
+	if (val) return val;
+	val = f->hour - s->hour;
+	if (val) return val;
+	val = f->minute - s->minute;
+	if (val) return val;
+	return 0;
+}
+
+void init_schedule_slot(char *line, struct elf_schedule* sched){
+	int i = 0;
+	bool donullparam = false;
+	char *secondhalf = NULL, *ptr = NULL;
+	while((ptr = strtok(!donullparam ? line : NULL, DELIMS))){
+		switch(i){
+		case 0:
+			donullparam = true;
+			sched->year = atoi(ptr);
+			break;
+		case 1:
+			sched->month = atoi(ptr);
+			break;
+		case 2:
+			sched->day = atoi(ptr); 
+			break;
+		case 3:
+			sched->hour = atoi(ptr);
+			break;
+		case 4:
+			sched->minute = atoi(ptr);
+			break;
+		case 5:
+			//at this point the data begins...
+			//Guard #x begins shift
+			//'falls asleep'
+			//'wakes up'
+			if (!strcmp(ptr, "wakes")){
+//				sched->begin = false;
+				sched->asleep = false;	
+				sched->id = 0;
+			}
+			else if (!strcmp(ptr, "falls")){
+				sched->asleep = true;
+				sched->id = 0;
+			}
+			else if (!strcmp(ptr, "Guard")){
+				//get the guards number by calling strcmp once more.
+				ptr = strtok(NULL, DELIMS);
+				sched->id = atoi(ptr);
+			}
+			//at this point we have all the data we need,
+			//don't bother to strtok the rest of the string
+			//goto done;
+			return;
+		}
+		i++;
+	}
+}
+
+int find_best_elf(struct elf_schedule* scheds, int schedsize){
+	int i, gsize = 0;
+//	int guards[100];
+	for (i = 0; i < schedsize; i++){
+//		if (scheds[i] != 0){
+			
+//		}
+	}
+}
+
+int main(int argc, char **argv){
+	int curr_line = 0;
+	int size = FILELINE;
+	char *line = NULL;
+	ssize_t n = 0;
+	FILE *input_file;
+	struct elf_schedule *sched = malloc(sizeof(struct elf_schedule) *FILELINE);
+	if (!sched){
+		fprintf(stderr, "OUT OF MEMORY\n");
+		exit(1);
+	}
+	if (argc != 2){
+		fprintf(stderr, "Please provide input file.\n");
+		exit(1);
+	}
+	input_file = fopen(argv[1], "r");
+	while(getline(&line, &n, input_file) != -1){
+		init_schedule_slot(line, &(sched[curr_line]));
+		curr_line++;
+		if (curr_line>size){
+			size *= 2;
+			sched = realloc(sched, sizeof(struct elf_schedule)*2);
+			if (!sched){
+				fprintf(stderr, "OUT OF MEMORY\n");
+				exit(1);
+			}
+		}
+	}
+	qsort(sched, size, sizeof(struct elf_schedule), compare_scheds);
+	printf("solution: %d\n", find_best_elf(sched, size));
+	free(sched);
+	//getline allocates for us
+	free(line);
+	return 0;
+}
